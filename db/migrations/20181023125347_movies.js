75cb8ac0fed28f4c04a1421c7ef8e97898dc597f
nicholasgriffen <nicholas.s.griffen@gmail.com>
2018-10-24 21:31:58 -0600

    increment updated_at

--- db/migrations/20181023125347_movies.js
+++ db/migrations/20181023125347_movies.js
@@ -2,7 +2,8 @@
 exports.up = function (knex) {
     return knex.schema.createTable('movies', table => {
         table.increments()
-        table.timestamps(true, true)
+        table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
+        table.timestamp('created_at').defaultTo(knex.fn.now())
         table.string('title').notNullable()
         table.string('director').notNullable()
         table.integer('year').notNullable()
