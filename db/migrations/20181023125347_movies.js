c9d6a25ea17c8137f8a6d40e7f7e442a88ab237b
nicholasgriffen <nicholas.s.griffen@gmail.com>
2018-10-24 21:34:16 -0600

    didn't work

--- db/migrations/20181023125347_movies.js
+++ db/migrations/20181023125347_movies.js
@@ -2,8 +2,7 @@
 exports.up = function (knex) {
     return knex.schema.createTable('movies', table => {
         table.increments()
-        table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
-        table.timestamp('created_at').defaultTo(knex.fn.now())
+        table.timestamps(true, true)
         table.string('title').notNullable()
         table.string('director').notNullable()
         table.integer('year').notNullable()
